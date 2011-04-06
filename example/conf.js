c47e1fe7662e11affc558751706329fde7561ce4
bnoguchi <brian.noguchi@gmail.com>
2011-04-05 19:58:40 -0700

    Added example

---
+++ example/conf.js
@@ -0,0 +1,6 @@
+module.exports = {
+    fb: {
+        appId: '111565172259433'
+      , appSecret: '85f7e0a0cc804886180b887c1f04a3c1'
+    }
+};
---
+++ example/server.js
@@ -0,0 +1,83 @@
+var express = require('express');
+
+var conf = require('./conf');
+
+var everyauth = require('everyauth')
+  , Promise = everyauth.Promise;
+
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+  , ObjectId = mongoose.SchemaTypes.ObjectId;
+
+var UserSchema = new Schema({})
+  , User;
+
+var mongooseAuth = require('../index');
+
+UserSchema.plugin(mongooseAuth, {
+    facebook: {
+      everyauth: {
+          myHostname: 'http://local.host:3000'
+        , appId: conf.fb.appId
+        , appSecret: conf.fb.appSecret
+        , redirectPath: '/'
+        , findOrCreateUser: function (sess, accessTok, fbUser) {
+            var promise = new Promise();
+            User.findOne({'fb.id': fbUser.id}, function (err, foundUser) {
+              if (foundUser)
+                return promise.fulfill(foundUser);
+              User.createWithFB(fbUser, accessTok, function (err, createdUser) {
+                return promise.fulfill(createdUser);
+              });
+            });
+        } 
+      }
+    }
+  , password: {
+        everyauth: {
+            getLoginPath: '/login'
+          , postLoginPath: '/login'
+          , loginView: 'login.jade'
+          , getRegisterPath: '/register'
+          , postRegisterPath: '/register'
+          , registerView: 'register.jade'
+          , redirectPath: '/'
+          , User: function () {
+              return User;
+            }
+        }
+    }
+});
+// Adds login: String
+
+mongoose.model('User', UserSchema);
+
+mongoose.connect('mongodb://localhost/example');
+
+User = mongoose.model('User');
+
+var app = express.createServer(
+    express.bodyParser()
+  , express.static(__dirname + "/public")
+  , express.cookieParser()
+  , express.session({ secret: 'esoognom'})
+  , mongooseAuth.middleware()
+);
+
+app.configure( function () {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+});
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+mongooseAuth.helpExpress(app);
+
+app.listen(3000);
