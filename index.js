47e1bfce12dddfc07238120a1670975bf611891f
appr <appr@appr.me>
2012-02-23 20:57:47 +0400

    Vkontakte auth support
    Add support for Vkontakte server authorization based on the OAuth2.0
    protocol

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ var Modules = {
   , github: require('./lib/modules/github')
   , instagram: require('./lib/modules/instagram')
   , google: require('./lib/modules/google')
+  , vkontakte: require('./lib/modules/vkontakte')
 };
 
 // Mostly, we need this because password needs to be loaded before everything else
 // so that other modules can use everyauth.password.loginKey()
-var moduleLoadOrder = ['everymodule', 'password', 'facebook', 'twitter', 'github', 'instagram', 'google'];
+var moduleLoadOrder = ['everymodule', 'password', 'facebook', 'twitter', 'github', 'instagram', 'google', 'vkontakte'];
 
 /**
  * Decorates the (User) Schema with the proper attributes.
